2728c0ae56bc870f404a722ecc1378088328bf72
selimmemduhoglu <mselimmemduhoglu@gmail.com>
2024-07-16 23:30:14 +0300

    Add project files.

---
+++ CatchQueueModel/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace CatchQueueModel
+{
+    internal class Program
+    {
+        static string key = "message-queue";
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://hlxfilsm:z8HI9n6M0caNW4Gr1YQmrhCic56U1pTG@toad.rmq.cloudamqp.com/hlxfilsm");
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare(key, true, false, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(key, true, consumer);
+
+            consumer.Received += Consumer_Received;
+
+            Console.ReadLine();
+
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            Console.WriteLine("Received Message: " + Encoding.UTF8.GetString(e.Body.ToArray()));
+        }
+    }
+}
---
+++ RabbitMqBasicExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMqBasicExample
+{
+    internal class Program
+    {
+        static string key = "message-queue";
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                SendMessage();
+            }
+        }
+
+        public static void SendMessage()
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://hlxfilsm:z8HI9n6M0caNW4Gr1YQmrhCic56U1pTG@toad.rmq.cloudamqp.com/hlxfilsm");
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare(key, true, false, false);
+            var message = "Test Message";
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(String.Empty, key, null, body);
+
+            Console.WriteLine("Hello world!");
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
